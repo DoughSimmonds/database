403368981db33ba5688de30ae20f58ba7c3759e3
Salja <salja2012@hotmail.de>
2012-08-30 12:31:38 +0200

    Sybc with last MaNGOS [12151] and ScriptDev2 [2701]
    Signed-off-by: Salja <salja2012@hotmail.de>

--- mangos/db_version.sql
+++ mangos/db_version.sql
@@ -38,7 +38,7 @@ CREATE TABLE `db_version` (
   `version` varchar(120) DEFAULT NULL,
   `creature_ai_version` varchar(120) DEFAULT NULL,
   `cache_id` int(10) DEFAULT '0',
-  `required_12141_02_mangos_mangos_command` bit(1) DEFAULT NULL
+  `required_12150_01_mangos_mangos_string` bit(1) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 ROW_FORMAT=FIXED COMMENT='Used DB version notes';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -48,7 +48,7 @@ CREATE TABLE `db_version` (
 
 LOCK TABLES `db_version` WRITE;
 /*!40000 ALTER TABLE `db_version` DISABLE KEYS */;
-INSERT INTO `db_version` (`version`, `creature_ai_version`, `cache_id`, `required_12141_02_mangos_mangos_command`) VALUES
+INSERT INTO `db_version` (`version`, `creature_ai_version`, `cache_id`, `required_12150_01_mangos_mangos_string`) VALUES
 ('MaNGOS Cataclysm Patch 4.3.4 Database','ACID 3.1.0 \'Another Wild Adventure\' - Full Release for CMaNGOS (3.3.5a Client)',404,NULL);
 /*!40000 ALTER TABLE `db_version` ENABLE KEYS */;
 UNLOCK TABLES;
